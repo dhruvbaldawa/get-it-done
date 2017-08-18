141039550f880fd451ca3fbb8a0b7df10dba0c27
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2017-08-18 22:48:23 +0400

    add state machine and state transitions

--- get_it_done/models.py
+++ get_it_done/models.py
@@ -1,11 +1,15 @@
 from datetime import datetime
+from functools import partial
 
+import logging
 from peewee import *
 from playhouse.postgres_ext import ArrayField
+from transitions import Machine
 
-from .constants import MINUTES
+from .constants import MINUTES, DAYS
 from .database import database
 
+logger = logging.getLogger(__name__)
 
 class BaseModel(Model):
     created_at = TimestampField(default=datetime.now)
@@ -59,10 +63,57 @@ class GmailThread(BaseModel):
     id = CharField(unique=True, primary_key=True)
     user = ForeignKeyField(OAuthUser, related_name='gmail_threads')
     labels = ArrayField(field_class=CharField, default=[])
-    state = CharField(default='initial')
+    state = CharField(default=STATE_INITIAL)
     sent_at = TimestampField(default=0)
 
-
+    def _generate_transitions(self):
+        return [
+            {
+                'trigger': 'next',
+                'source': [self.STATE_INITIAL, self.STATE_14D, self.STATE_ARCHIVED],
+                'dest': self.STATE_ARCHIVED,
+                'conditions': partial(self.has_time_elapsed, '15d'),
+                'after': self.archive,
+            },
+        ] + [
+            {
+                'trigger': 'next',
+                'source': self.STATE_INITIAL,
+                'dest': state,
+                'conditions': partial(self.has_time_elapsed, state),
+                'after': partial(self.change_labels, self.STATE_INITIAL, state),
+            }
+            for state in reversed((self.STATE_1D, self.STATE_3D, self.STATE_7D, self.STATE_10D, self.STATE_14D))
+        ] + [
+            {
+                'trigger': 'next',
+                'source': source,
+                'dest': destination,
+                'conditions': partial(self.has_time_elapsed, destination),
+                'after': partial(self.change_labels, source, destination),
+            }
+            for source, destination in zip(
+                (self.STATE_INITIAL, self.STATE_1D, self.STATE_3D, self.STATE_7D, self.STATE_10D),
+                (self.STATE_1D, self.STATE_3D, self.STATE_7D, self.STATE_10D, self.STATE_14D),
+            )
+        ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.machine = Machine(
+            model=self,
+            states=self.STATES,
+            initial=self.STATE_INITIAL,
+            transitions=self._generate_transitions(),
+            after_state_change='save',
+        )
+
+    def has_time_elapsed(self, time):
+        time = int(time[:-1])
+        return (datetime.now() - self.sent_at).total_seconds() > time * DAYS
+
+    def change_labels(self, source, destination):
+        logger.debug('changing labels from %s to %s', source, destination)
+
+    def archive(self):
+        logger.debug('Archiving %s', self.id)
